6f6edc7af1cf890f9091a8d762619855c8f4515c
Zheng <zengzheng@gmail.com>
2014-11-03 09:22:56 +0100

    added: folder Meilenstein2;music_json.j; favoriten.html;readme.txt Note: html is not completed-Zheng

---
+++ Meilenstein3/JS/music_json.js
@@ -0,0 +1,103 @@
+
+var data= {
+	"Alben":[
+      {
+        "Interpreter": "Farin Urlaub",
+        "Albumtitel": "Faszination Weltraum",
+        "Erscheinungsjahr": "2014",
+        "Genre": "Punk"
+      },
+      {
+        "Interpreter": "Die Äzte",
+        "Albumtitel": "Planet Punk",
+        "Erscheinungsjahr": "1995",
+        "Genre": "Punk"
+      },
+	   {
+        "Interpreter": "The Offspring",
+        "Albumtitel": "Smash",
+        "Erscheinungsjahr": "1994",
+        "Genre": "Punk"
+      }, 
+	  {
+        "Interpreter": "Blink 182",
+        "Albumtitel": "Enema of the State",
+        "Erscheinungsjahr": "1999",
+        "Genre": "Punk"
+      }, 
+	  {
+        "Interpreter": "Rise Against",
+        "Albumtitel": "Siren Song of the Counter Culture",
+        "Erscheinungsjahr": "2004",
+        "Genre": "Punk"
+      }, 
+	  {
+        "Interpreter": "Vampire Weekend",
+        "Albumtitel": "Vampire Weekend",
+        "Erscheinungsjahr": "2007",
+        "Genre": "Punk"
+      },
+	   {
+        "Interpreter": "Bouncing Souls",
+        "Albumtitel": "How I spent my summer vacation",
+        "Erscheinungsjahr": "2001",
+        "Genre": "Punk"
+      },
+	   {
+        "Interpreter": "Beatsteaks",
+        "Albumtitel": "Living Targets",
+        "Erscheinungsjahr": "2002",
+        "Genre": "Punk"
+      },
+	   {
+        "Interpreter": "Bloc Party",
+        "Albumtitel": "Silent Alarm",
+        "Erscheinungsjahr": "2005",
+        "Genre": "Punk"
+      },
+	   {
+        "Interpreter": "WIZO",
+        "Albumtitel": "Anderster",
+        "Erscheinungsjahr": "2004",
+        "Genre": "Punk"
+      }
+	  ]
+}
+//append table heads and data to the table
+function buildTable(){
+	var myTable = document.getElementById('myTable');
+	addTableHeads(myTable);
+	var heads = getHeads();
+	for (var j=0;j<data.Alben.length;j++){
+		var tr= document.createElement('tr').cloneNode(false);
+		for(var i = 0;	i<heads.length;i++){
+			var td = document.createElement('td').cloneNode(false);
+			td.appendChild(document.createTextNode(data.Alben[j][heads[i]]));
+			tr.appendChild(td);
+			}
+		myTable.appendChild(tr);
+	}
+}
+
+function addTableHeads(table){
+	var tr= document.createElement('tr').cloneNode(false);
+	var heads = getHeads(data);
+	for(var i = 0;	i<heads.length;i++){
+		var th = document.createElement('th').cloneNode(false);
+		th.appendChild(document.createTextNode(heads[i]));
+		tr.appendChild(th);
+	}
+	table.appendChild(tr);
+}
+// get all the property keys from JSON data
+function getHeads(){
+	// headers is going to be returned as array
+	var heads = new Array();
+	// looke into one entity in albums and collect header
+	var album= data.Alben[0];
+	for(var key in album){
+		heads.push(key);
+	}
+	return heads;
+}
+
---
+++ Meilenstein3/readme.txt
@@ -0,0 +1,4 @@
+Die Dokumente in Meilenstein2 werden nach Testat verbessert und ins hier kopiert. 
+1, music und film haben jetzt nur ein css.
+2, Bilder sind in einem getrennten Verzeichnis.
+3, Doppelte ID in html und andere Fehler werden beseitigt. 
