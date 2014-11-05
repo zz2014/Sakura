d0fe8c307f04587a22310fcdaa0de8303bc2070e
Thalla <temerita16@gmail.com>
2014-11-05 13:03:21 +0100

    film_json, favorites.css, favorites.html, divs added, Task 3 -Thalla

---
+++ Meilenstein3/JS/film_json.js
@@ -0,0 +1,75 @@
+
+var filmdata= {
+	"Filme":[
+      {
+        "Filmtitel": "AClockwork Orange",
+        "Regie": "Stanley",
+        "Drehbuch": "Stanley Kubrick",
+        "Erscheinungsjahr": "1971",
+		"Genre": "Komödie"
+      },
+      {
+        "Filmtitel": "Love Exposure",
+        "Regie": "Haruo Umekawa",
+        "Drehbuch": "Sion Sono",
+        "Erscheinungsjahr": "2008",
+		"Genre": "Psycho"
+      },
+	   {
+        "Filmtitel": "Richy Guitar",
+        "Regie": "Michael Laux",
+        "Drehbuch": "Michael Laux",
+        "Erscheinungsjahr": "1995",
+		"Genre": "Komodie"
+      }, 
+	  {
+        "Filmtitel": "Inglorious Basterds",
+        "Regie": "Quentin Tarantino",
+        "Drehbuch": "Quentin Tarantino",
+        "Erscheinungsjahr": "2009",
+		"Genre": "Komödie"
+      }, 
+	  {
+        "Filmtitel": "Pulp Fiction",
+        "Regie": "Quentin Tarantino",
+        "Drehbuch": "Quentin Tarantino",
+        "Erscheinungsjahr": "1994",
+		"Genre": "Psycho"
+      }, 
+	  {
+        "Filmtitel": "Léon - der Profi",
+        "Regie": "Luc Besson",
+        "Drehbuch": "Luc Besson",
+        "Erscheinungsjahr": "1994",
+		"Genre": "Romanze"
+      },
+	   {
+        "Filmtitel": "Das Schicksal ist ein mieser Verräter",
+        "Regie": "Josh Boone",
+        "Drehbuch": "Scott Neustädter",
+        "Erscheinungsjahr": "2014",
+		"Genre": "Romanze"
+      },
+	   {
+        "Filmtitel": "",
+        "Regie": "",
+        "Drehbuch": "",
+        "Erscheinungsjahr": "",
+		"Genre": ""
+      },
+	   {
+        "Filmtitel": "",
+        "Regie": "",
+        "Drehbuch": "",
+        "Erscheinungsjahr": "",
+		"Genre": ""
+      },
+	   {
+        "Filmtitel": "",
+        "Regie": "",
+        "Drehbuch": "",
+        "Erscheinungsjahr": "",
+		"Genre": ""
+      }
+	  ]
+}
